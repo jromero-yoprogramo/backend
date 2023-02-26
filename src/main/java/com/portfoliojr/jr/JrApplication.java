444dfa76919a9b6f2ef2eae21d04f86731cecd40
jromero-yoprogramo <jromero.yoprogramo@gmail.com>
2023-02-26 17:28:33 -0300

    nuevo intento deploy

--- src/main/java/com/portfoliojr/jr/JrApplication.java
+++ src/main/java/com/portfoliojr/jr/JrApplication.java
@@ -1,8 +1,8 @@
 package com.portfoliojr.jr;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class JrApplication {
