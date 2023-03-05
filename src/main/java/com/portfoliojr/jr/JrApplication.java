30038ddf1dbc3ae8e10b56e680960fa386aa1f7c
jromero-yoprogramo <jromero.yoprogramo@gmail.com>
2023-03-05 12:50:44 -0300

    nuevo intento de deploy

--- src/main/java/com/portfoliojr/jr/JrApplication.java
+++ src/main/java/com/portfoliojr/jr/JrApplication.java
@@ -3,6 +3,9 @@ package com.portfoliojr.jr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
 
 @SpringBootApplication
 public class JrApplication {
@@ -10,5 +13,11 @@ public class JrApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JrApplication.class, args);
 	}      
+        
+        @Bean(name = "mvcHandlerMappingIntrospector")
+	public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+		return new HandlerMappingIntrospector();
+	}
              
 }
+
