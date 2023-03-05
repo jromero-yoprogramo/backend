40917ab9ff65fc135690927ecba49111acc8ed4e
jromero-yoprogramo <jromero.yoprogramo@gmail.com>
2023-03-05 12:28:40 -0300

    nuevo intento de deploy
    especifique el main class que aparentemente no lo encontraba.

--- src/main/java/com/portfoliojr/jr/JrApplication.java
+++ src/main/java/com/portfoliojr/jr/JrApplication.java
@@ -3,19 +3,9 @@ package com.portfoliojr.jr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-//@SpringBootApplication
-//public class JrApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(JrApplication.class, args);
-//	}      
-//             
-//}
 
 @SpringBootApplication
-public class JrApplication extends SpringBootServletInitializer{
+public class JrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JrApplication.class, args);
