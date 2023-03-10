49880c7b0f2528f9da1e87610aa3e35a60104f90
jromero-yoprogramo <jromero.yoprogramo@gmail.com>
2023-03-10 11:06:06 -0300

    deploy en Railway app
    funciona completamente sin hacer cambios en la configuracion. En Render failed con ningun aviso de error.

--- src/main/java/com/portfoliojr/jr/Controller/CProyecto.java
+++ src/main/java/com/portfoliojr/jr/Controller/CProyecto.java
@@ -21,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://jrfrontend-40145.web.app/")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CProyecto {
 
     @Autowired
