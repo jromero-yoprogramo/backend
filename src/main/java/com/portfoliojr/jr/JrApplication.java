09542383b8c46afc9c20e500b8b1fd3f06041b05
jromero-yoprogramo <jromero.yoprogramo@gmail.com>
2023-03-12 11:53:28 -0300

    intento deploy

--- src/main/java/com/portfoliojr/jr/JrApplication.java
+++ src/main/java/com/portfoliojr/jr/JrApplication.java
@@ -1,14 +1,14 @@
 package com.portfoliojr.jr;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.ScheduledFuture;
+//import java.util.HashSet;
+//import java.util.Set;
+//import java.util.concurrent.ScheduledFuture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.scheduling.Trigger;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+//import org.springframework.scheduling.Trigger;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
@@ -21,28 +21,28 @@ public class JrApplication {
 
     }
 
-    public ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler() {
-        private final Set<ScheduledFuture<?>> futures = new HashSet<>();
-
-        @Override
-        public ScheduledFuture<?> scheduleWithFixedDelay(Runnable task, long delay) {
-            ScheduledFuture<?> future = super.scheduleWithFixedDelay(task, delay);
-            futures.add(future);
-            return future;
-        }
-
-        @Override
-        public ScheduledFuture<?> schedule(Runnable task, Trigger trigger) {
-            ScheduledFuture<?> future = super.schedule(task, trigger);
-            futures.add(future);
-            return future;
-        }
-
-        @Override
-        public void shutdown() {
-            futures.forEach(future -> future.cancel(true));
-            super.shutdown();
-        }
-
-    };
+//    public ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler() {
+//        private final Set<ScheduledFuture<?>> futures = new HashSet<>();
+//
+//        @Override
+//        public ScheduledFuture<?> scheduleWithFixedDelay(Runnable task, long delay) {
+//            ScheduledFuture<?> future = super.scheduleWithFixedDelay(task, delay);
+//            futures.add(future);
+//            return future;
+//        }
+//
+//        @Override
+//        public ScheduledFuture<?> schedule(Runnable task, Trigger trigger) {
+//            ScheduledFuture<?> future = super.schedule(task, trigger);
+//            futures.add(future);
+//            return future;
+//        }
+//
+//        @Override
+//        public void shutdown() {
+//            futures.forEach(future -> future.cancel(true));
+//            super.shutdown();
+//        }
+//
+//    };
 }
--- src/main/java/com/portfoliojr/jr/Security/MainSecurity.java
+++ src/main/java/com/portfoliojr/jr/Security/MainSecurity.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import static org.springframework.security.config.Customizer.withDefaults;
+//import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
