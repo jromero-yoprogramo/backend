a1b9931f8f79d0bd5ed31ec6a51ca29b5a759305
jromero-yoprogramo <jromero.yoprogramo@gmail.com>
2023-03-12 12:13:24 -0300

    intento deploy

---
+++ src/main/java/com/portfoliojr/jr/WebConfig.java
@@ -0,0 +1,18 @@
+
+package com.portfoliojr.jr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/** * Clase que habilita CORS * @author JR */
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    public void addCorsMapping(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
+}
