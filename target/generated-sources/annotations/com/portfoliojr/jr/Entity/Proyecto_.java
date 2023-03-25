6a4b4125990803138781b97d23067a027b101e55
jromero-yoprogramo <jromero.yoprogramo@gmail.com>
2023-03-25 11:28:58 -0300

    corregi error

--- target/generated-sources/annotations/com/portfoliojr/jr/Entity/Proyecto_.java
+++ target/generated-sources/annotations/com/portfoliojr/jr/Entity/Proyecto_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-03-25T11:21:07", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-03-25T11:28:38", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Proyecto.class)
 public class Proyecto_ { 
 
